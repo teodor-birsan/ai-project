096f6f1a1520fd99178e5630fd28e88d3f7ef7d3
Minimatrix26 <armand.brudea@yahoo.com>
2025-04-05 16:20:26 +0300

    added PPOSettings
    added PPOSettings and changed Optimizer -> TorchOptimizer parameter in class CustomOptimizer

--- AI Project - Python/trainer_plugins/ppo/ppo_optimizer.py
+++ AI Project - Python/trainer_plugins/ppo/ppo_optimizer.py
@@ -1,9 +1,9 @@
 from typing import Dict
 
 from mlagents.trainers.buffer import AgentBuffer
-from mlagents.trainers.optimizer.torch_optimizer import Optimizer
+from mlagents.trainers.optimizer.torch_optimizer import TorchOptimizer
 
 
-class CustomOptimizer(Optimizer):
+class CustomOptimizer(TorchOptimizer):
     def update(self, batch: AgentBuffer, num_sequences: int) -> Dict[str, float]:
         pass
