7ce4bdf7d9553c8a77ebf45df12c04149f65e22a
Minimatrix26 <armand.brudea@yahoo.com>
2025-04-13 17:47:33 +0300

    ppo optimizer improvement
    now the agent trains without crashes

--- AI Project - Python/trainer_plugins/trainers/ppo/ppo_optimizer.py
+++ AI Project - Python/trainer_plugins/trainers/ppo/ppo_optimizer.py
@@ -1,11 +1,15 @@
 from typing import Dict, cast
 from mlagents.torch_utils import default_device, torch
-from mlagents.trainers.buffer import AgentBuffer, BufferKey
+from mlagents.trainers.buffer import AgentBuffer, BufferKey, RewardSignalUtil
 from mlagents.trainers.optimizer.torch_optimizer import TorchOptimizer
 from mlagents.trainers.policy.torch_policy import TorchPolicy
 from mlagents.trainers.settings import TrainerSettings
+from mlagents.trainers.torch_entities.action_log_probs import ActionLogProbs
+from mlagents.trainers.torch_entities.agent_action import AgentAction
 from mlagents.trainers.torch_entities.networks import ValueNetwork
 from mlagents.trainers.torch_entities.utils import ModelUtils
+from mlagents.trainers.trajectory import ObsUtil
+from mlagents_envs.timers import timed
 
 from .custom_ppo_settings import PPOSettings
 
@@ -63,24 +67,52 @@ class CustomOptimizer(TorchOptimizer):
 
         self.stream_names = list(self.reward_signals.keys())
 
+    @timed
     def update(self, batch: AgentBuffer, num_sequences: int) -> Dict[str, float]:
-        # Use precomputed decay values instead of ModelUtils.schedule_value
-        decay_eps = self.decay_epsilon.get_value(self.policy.get_current_step())
-        decay_beta = self.decay_beta.get_value(self.policy.get_current_step())
+        """
+        Performs update on model.
+        :param batch: Batch of experiences.
+        :param num_sequences: Number of sequences to process.
+        :return: Results of update.
+        """
+        # Get decayed parameters
         decay_lr = self.decay_learning_rate.get_value(self.policy.get_current_step())
+        decay_eps = self.decay_epsilon.get_value(self.policy.get_current_step())
+        decay_bet = self.decay_beta.get_value(self.policy.get_current_step())
+        returns = {}
+        old_values = {}
+        for name in self.reward_signals:
+            old_values[name] = ModelUtils.list_to_tensor(
+                batch[RewardSignalUtil.value_estimates_key(name)]
+            )
+            returns[name] = ModelUtils.list_to_tensor(
+                batch[RewardSignalUtil.returns_key(name)]
+            )
 
-        ModelUtils.update_learning_rate(self.optimizer, decay_lr)
+        n_obs = len(self.policy.behavior_spec.observation_specs)
+        current_obs = ObsUtil.from_buffer(batch, n_obs)
+        # Convert to tensors
+        current_obs = [ModelUtils.list_to_tensor(obs) for obs in current_obs]
 
-        current_obs = ModelUtils.list_to_tensor(batch[BufferKey.OBSERVATIONS])
-        actions = ModelUtils.list_to_tensor(batch[BufferKey.ACTIONS])
-        old_log_probs = ModelUtils.list_to_tensor(batch[BufferKey.LOG_PROBS])
-        advantages = ModelUtils.list_to_tensor(batch[BufferKey.ADVANTAGES])
-        target_values = ModelUtils.list_to_tensor(batch[BufferKey.RETURNS])
-        act_masks = ModelUtils.list_to_tensor(batch[BufferKey.MASKS])
-        loss_masks = ModelUtils.list_to_tensor(batch[BufferKey.MASKS])
+        act_masks = ModelUtils.list_to_tensor(batch[BufferKey.ACTION_MASK])
+        actions = AgentAction.from_buffer(batch)
 
-        memories = ModelUtils.list_to_tensor(batch.get(BufferKey.MEMORY, []))
-        value_memories = ModelUtils.list_to_tensor(batch.get(BufferKey.MEMORY, []))
+        memories = [
+            ModelUtils.list_to_tensor(batch[BufferKey.MEMORY][i])
+            for i in range(0, len(batch[BufferKey.MEMORY]), self.policy.sequence_length)
+        ]
+        if len(memories) > 0:
+            memories = torch.stack(memories).unsqueeze(0)
+
+        # Get value memories
+        value_memories = [
+            ModelUtils.list_to_tensor(batch[BufferKey.CRITIC_MEMORY][i])
+            for i in range(
+                0, len(batch[BufferKey.CRITIC_MEMORY]), self.policy.sequence_length
+            )
+        ]
+        if len(value_memories) > 0:
+            value_memories = torch.stack(value_memories).unsqueeze(0)
 
         run_out = self.policy.actor.get_stats(
             current_obs,
@@ -89,35 +121,67 @@ class CustomOptimizer(TorchOptimizer):
             memories=memories,
             sequence_length=self.policy.sequence_length,
         )
+
         log_probs = run_out["log_probs"]
         entropy = run_out["entropy"]
 
-        values, _ = self._critic.critic_pass(  # Fixed: use self._critic instead of self.policy.critic
+        values, _ = self.critic.critic_pass(
             current_obs,
             memories=value_memories,
             sequence_length=self.policy.sequence_length,
         )
-
-        value_loss = ModelUtils.masked_mean((target_values - values) ** 2, loss_masks)
-
-        policy_loss = ModelUtils.trust_region_policy_loss(
-            advantages, log_probs, old_log_probs, loss_masks, decay_eps
+        old_log_probs = ActionLogProbs.from_buffer(batch).flatten()
+        log_probs = log_probs.flatten()
+        loss_masks = ModelUtils.list_to_tensor(batch[BufferKey.MASKS], dtype=torch.bool)
+        value_loss = ModelUtils.trust_region_value_loss(
+            values, old_values, returns, decay_eps, loss_masks
         )
-
+        policy_loss = ModelUtils.trust_region_policy_loss(
+            ModelUtils.list_to_tensor(batch[BufferKey.ADVANTAGES]),
+            log_probs,
+            old_log_probs,
+            loss_masks,
+            decay_eps,
+        )
         loss = (
                 policy_loss
                 + 0.5 * value_loss
-                - decay_beta * ModelUtils.masked_mean(entropy, loss_masks)
+                - decay_bet * ModelUtils.masked_mean(entropy, loss_masks)
         )
 
+        # Set optimizer learning rate
+        ModelUtils.update_learning_rate(self.optimizer, decay_lr)
         self.optimizer.zero_grad()
         loss.backward()
-        self.optimizer.step()
 
-        return {
+        self.optimizer.step()
+        update_stats = {
+            # NOTE: abs() is not technically correct, but matches the behavior in TensorFlow.
+            # TODO: After PyTorch is default, change to something more correct.
             "Losses/Policy Loss": torch.abs(policy_loss).item(),
             "Losses/Value Loss": value_loss.item(),
             "Policy/Learning Rate": decay_lr,
             "Policy/Epsilon": decay_eps,
-            "Policy/Beta": decay_beta,
+            "Policy/Beta": decay_bet,
         }
+
+        return update_stats
+
+    def get_modules(self):
+        modules = {
+            "Optimizer:value_optimizer": self.optimizer,
+            "Optimizer:critic": self._critic,
+        }
+        for reward_provider in self.reward_signals.values():
+            modules.update(reward_provider.get_modules())
+
+        return modules
+
+    @property
+    def critic(self):
+        # Only return critic if it implements update_normalization
+        if hasattr(self._critic, "update_normalization"):
+            return self._critic
+        else:
+            raise NotImplementedError("Critic does not support normalization.")
+
